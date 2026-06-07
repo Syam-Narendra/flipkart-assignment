290ba141c45434534989585d1f0a8986b936ccb8
Syam <syamnarendra85@gmail.com>
2026-06-07 23:46:49 +0530

    fix

--- backend/main.py
+++ backend/main.py
@@ -22,7 +22,7 @@ app = FastAPI(
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173", "http://localhost:3000", "https://flipkart-assignment.onrender.com"],
+    allow_origins=["http://localhost:5173", "http://localhost:3000", "https://syam-flipkart-assignment.vercel.app/"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
